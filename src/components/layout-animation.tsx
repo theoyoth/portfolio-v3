cbdb0a7a2b7630d098295f28d0c7bb59490fbf39
theoyoth <theobantang1@gmail.com>
2024-08-09 10:45:56 +0800

    animation layout

---
+++ src/components/layout-animation.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import {motion} from 'framer-motion';
+
+const LayoutAnimation = ({children,key}:{children:React.ReactNode,key?:any}) => {
+
+  const animation = {
+    hidden:{
+      y:"20px",
+      opacity:0,
+    },
+    show:{
+      y:0,
+      opacity:1,
+      transition:{
+        duration:0.3,
+        ease:'easeOut'
+      }
+    },
+    exit:{
+      y:"20px",
+      opacity:0,
+      transition:{
+        duration:0.3,
+        ease:'easeOut'
+      }
+    },
+  }
+  return (
+      <motion.div 
+        variants={animation} 
+        initial='hidden'
+        animate='show'
+        exit='exit'
+      >
+        {children}
+      </motion.div>
+  )
+}
+
+export default LayoutAnimation
+
